634138a6b4eb98b42fdc630e823971c2f5512cca
User <mike7892@mail.ru>
2023-05-09 09:36:57 +0300

    add print

---
+++ lesson_3.py
@@ -0,0 +1,121 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = 'Alex'
+# age = 35
+# print('Hello ' + name + ' your age ' + str(35))
+# print('Hello %s your age %d' % (name, age))
+# print('Hello {first_name} your age {user_age}'.format(first_name=name, user_age=age))
+# print(f'Hello {name} your age {age}')
+#
+#
+# print(f'{35} \N{fire}')
+# print ('🔥')
+#
+#
+# text = 'hello---world---python'
+# print(text.split('---'))
+#
+# text = 'hello---world---python'
+# print(text.split('---', 1))
+#
+# words = ('word', 'python', 'pycharm')
+# text = '---'.join(words)
+# print(text)
+#
+# text = 'hello'
+# print('-'.join(text))
+
+text = 'hello python world python'
+print(text.find('python', 10, 25))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
