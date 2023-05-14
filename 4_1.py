94a98cc9bd954b9ca2d062b3a2eaa89c9f831605
User <mike7892@mail.ru>
2023-05-14 20:29:02 +0300

    add print

--- 4_1.py
+++ 4_1.py
@@ -5,20 +5,28 @@
 # A=[2**i for i in range(1,N+1)]
 # print(A)
 #
-a = []
+# a = []
+#
+# N = int(input()) #пользователь сам вводит сколько чисел ему нужно
+#
+# for i in range(N):
+#
+#    a.append(2**i)
+#
+# print(a)
+#
+#for i in range(1, 10):
+    # for j in range(1, 10):
+    #     print(i * j, end="\t")
+    # print("\n")
+#
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# number = [number [i]*5 for i in range(len(number))]
+# print(number)
+#
+# test = float ( "inf" )
+#
+#
+# numbers = [i**2 for i in range(1, 101)]
+# print(numbers)
 
-N = int(input()) #пользователь сам вводит сколько чисел ему нужно
-
-for i in range(N):
-
-   a.append(2**i)
-
-print(a)
-
-
-
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-number = [number [i]*5 for i in range(len(number))]
-print(number)
-
-test = float ( "inf" )
