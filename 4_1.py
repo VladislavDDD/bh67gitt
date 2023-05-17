d8d7ca1a91a3d262813b983f48ea41de255912a3
User <mike7892@mail.ru>
2023-05-17 18:34:39 +0300

    add print

--- 4_1.py
+++ 4_1.py
@@ -1,32 +1,4 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n)
 
-
-# N=int(input())
-# A=[2**i for i in range(1,N+1)]
-# print(A)
-#
-# a = []
-#
-# N = int(input()) #пользователь сам вводит сколько чисел ему нужно
-#
-# for i in range(N):
-#
-#    a.append(2**i)
-#
-# print(a)
-#
-#for i in range(1, 10):
-    # for j in range(1, 10):
-    #     print(i * j, end="\t")
-    # print("\n")
-#
-# number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# number = [number [i]*5 for i in range(len(number))]
-# print(number)
-#
-# test = float ( "inf" )
-#
-#
-# numbers = [i**2 for i in range(1, 101)]
-# print(numbers)
-
+numbers = [i**2 for i in range(2, 101)]
+print(numbers)
